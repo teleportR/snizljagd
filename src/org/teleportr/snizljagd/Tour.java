bf0d1d794c13dd12fb0e790a3200e55131f4ad6e
atzepeng <jakob_buchetmann@hotmail.de>
2013-02-15 10:31:06 +0100

    Update src/org/teleportr/snizljagd/Tour.java

--- src/org/teleportr/snizljagd/Tour.java
+++ src/org/teleportr/snizljagd/Tour.java
@@ -29,8 +29,47 @@ public class Tour {
         "Apartment Schieß di glei weida: Am Ochsensepp 4, Berlin",
         "A&0 BerLin Mitte: KöpenickerStr. 127-129, Berlin",
         "aletto JugendhoteI Kreuzberg: Tempethofer Ufer 8/9, Berlin",
-        "atetto Ku'Damm Hostel: Hardenbergstr. 21, Berlin",
-        "atetto Schöneberg: Grunewaldstr. 33, Berlin",
+        "aletto Ku'Damm Hostel: Hardenbergstr. 21, Berlin",
+        "aletto Schöneberg: Grunewaldstr. 33, Berlin",
+        "Amstel House: Waldenser Str. 31, Berlin",
+        "AVS Hostel: Hohenzollerndamm 57-58, Berlin",
+        "BDP Gäste-Etage: Osloer Straße 12, Berlin",
+        "Berlin City Lounge: Pariser Str. 19, Berlin",
+        "CVJM Jugendgästehaus Berlin: Einemstr. 10, Berlin",
+        "Die Fabrik Hotel Kreuzberg: Schlesische Str. 18, Berlin",
+        "Grand Hostel Berlin: Tempelhofer Ufer 14, Berlin",
+        "Hostel Die Etage: Katharinenstr. 14, Berlin",
+        "Hostel HAppy Bed: Köernerstr. 53, Berlin",
+        "Hotel Arrival Berlin: Lietzenburger Straße 30, Berlin",
+        "Hotel Transit: Hagelberger Str. 53-54, Berlin",
+        "Inn-Berlin: Prinzenallee 49, Berlin",
+        "Jetpak Flashpacker Hostel: Pariser Str. 58, Berlin",
+        "JGH Mitte: Antwerpener Str. 40, Berlin",
+        "Hotel St. Michaels-Heim: Bismarckallee 23, berlin",
+        "Jugendgästehaus HBf: Leherter Str. 68, Berlin",
+        "Jugendherberge am Wannsee: Badeweg 1, Berlin",
+        "Jugendherberge Berlin: Kluckstr. 3, berlin",
+        "Main Station Hostel: Quitzowstr. 110, Berlin",
+        "Metropol Hostel: Mehringdamm 32-34, Berlin",
+        "Smarthostel Berlin: Genter Str. 53 A/B, Berlin",
+        "Sport Jugendhotel: Kaiserdamm 3, berlin",
+        "Sunshinehouse Berlin: Wexstr. 8, Berlin",
+        "Three little Pigs Hostel: Stresemannstraße 66, Berlin",
+        "Toms Gay Hostel: Kalckreuthstraße 10, Berlin",
+        "Western Comfort Hostelboot: Gröbenufer 8, Berlin",
+        "2A Hostel: Saalestr. 76, Berlin",
+        "A&O Berlin FH: Boxhagener Str. 76, Berlin",
+        "Alcatraz Hostel: Schönhauser Allee 133 A, Berlin",
+        "All Inn Hostel: Grünberger Str. 54, Berlin",
+        "Aurora Hostel: Pappelallee 21, Berlin",
+        "Backpacker Berlin: Knorrpromenade 10, Berlin",
+        "Check In Hostel: Markgrafenstr. 68, berlin",
+        
+        
+       
+        
+        
+        "
         
     };
     
